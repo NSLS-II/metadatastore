71d253e066b3ef8a563e50a2d84a06815b93de97
arkilic <arkilic@bnl.gov>
2014-08-05 09:35:34 -0400

    schema changes and dataapi.raw_commands.* modifications due to schema change

--- dummy.py
+++ dummy.py
@@ -1,42 +1,40 @@
 __author__ = 'arkilic'
-from metadataStore.dataapi.raw_commands import *
-#
 import time
 import random
+
+from metadataStore.dataapi.raw_commands import *
+
+
 h_id = random.randint(0, 200000)
 bc_id = random.randint(0, 450000)
 ev_d_id = random.randint(0, 200000)
 start = time.time()
-save_header(header_id=h_id, beamline_id='csx29', scan_id=1)
+save_header(beamline_id='csx29', scan_id=h_id)
 end = time.time()
 print('Header insert time is ' + str((end-start)*1000) + ' ms')
-
-
+#
 start = time.time()
-insert_event_descriptor(event_descriptor_id=ev_d_id, header_id=h_id, event_type_id=1, event_type_name='scan')
+insert_event_descriptor(scan_id=h_id, event_type_id=1, event_type_name='scan')
 end = time.time()
 print('Descriptor insert time is ' + str((end-start)*1000) + ' ms')
-
-
-
+#
 start = time.time()
-hdr = find(header_id='current', contents=True)
+insert_event(scan_id=h_id, descriptor_name='scan', owner='arkilic')
+end = time.time()
+print('Event insert time is ' + str((end-start)*1000) + ' ms')
+#
+start = time.time()
+hdr1 = find(scan_id='current', data=True)
 end = time.time()
 print('Header query time is '+str((end-start)*1000) + ' ms')
+#
+# start = time.time()
+# hdr2 = find(header_id='current', data=False)
+# print hdr2
+# end = time.time()
+# print('Header query time is '+str((end-start)*1000) + ' ms')
 
 start = time.time()
-save_beamline_config(beamline_cfg_id=bc_id, header_id=h_id, config_params={'hkl': [1, 1, 0]})
+hdr3 = find(scan_id=h_id, data=True)
+print hdr3[hdr3.keys()[0]]['event_descriptor_0']['events']
 end = time.time()
-print('BeamlineConfig insert time is '+str((end-start)*1000) + ' ms')
-
-
-find(owner='arki*')
-print find(owner='arkilic').keys()
-print find(beamline_id='csx29').keys()
-
-print find(start_time=datetime.datetime(2013,4, 4)).keys()
-
-print find(header_id='current')
-print find(header_id='last')
-print find(end_time=datetime.datetime(2013,4, 4))
-print find(start_time={'start': datetime.datetime(2012,1,1), 'end': datetime.datetime(2015,1,1)}).keys()
--- metadataStore/dataapi/raw_commands.py
+++ metadataStore/dataapi/raw_commands.py
@@ -10,27 +10,46 @@ from metadataStore.database.databaseTables import Header, BeamlineConfig, Event,
 from metadataStore.sessionManager.databaseInit import metadataLogger
 
 
-def save_header(header_id, scan_id, header_owner=getpass.getuser(), start_time=datetime.datetime.utcnow(), beamline_id=None,
+def save_header(scan_id, header_owner=getpass.getuser(), start_time=datetime.datetime.utcnow(), beamline_id=None,
                 status='In Progress',custom=dict()):
     """
-    >>> save_header(header_id=1345,scan_id=12)
-    >>> save_header(header_id=1345, scan_id=13, owner='arkilic')
-    >>> save_header(header_id=1345, scan_id=14, custom={'field1': 'value1', 'field2': 'value2'})
+    Saves a run header that serves as a container for even descriptors, beamline configurations, and events.
+    
+    :param scan_id: Consumer specified id for a specific scan
+    :type scan_id: int
+    :param header_owner: Run header owner (default: debian session owner)
+    :type header_owner: str
+    :param start_time: Run header create time
+    :type start_time: datetime object
+    :param beamline_id: Beamline Id
+    :type beamline_id: str
+    :param status: Run header completion status( In Progress/Complete
+    :type status: str
+    :param custom: Additional attribute value fields that can be user defined
+    :type custom: dict
+
+    >>> save_header(scan_id=12)
+    >>> save_header(scan_id=13, owner='arkilic')
+    >>> save_header(scan_id=14, custom={'field1': 'value1', 'field2': 'value2'})
+    >>> save_header(scan_id=15, owner='some owner', start_time=datetime.datetime(2014, 3, 4))
+    >>> save_header(scan_id=15, owner='some owner', beamline_id='csx')
+    >>> save_header(scan_id=15, owner='some owner', start_time=datetime.datetime(2014, 3, 4), beamline_id='csx')
+    >>> save_header(scan_id=15, start_time=datetime.datetime(2014, 3, 4), custom={'att': 'value'})
     """
-    if get_header_object(header_id):
-        raise ValueError('Header with given header_id exists')
-    else:
-        try:
-            header = Header(_id=header_id, owner=header_owner, start_time=start_time,
-                            end_time=start_time, beamline_id=beamline_id, scan_id=scan_id,
-                            custom=custom, status=status).save(wtimeout=100,write_concern={'w': 1})
-        except:
-            metadataLogger.logger.warning('Header cannot be created')
-            raise
+    try:
+        header = Header(owner=header_owner, start_time=start_time,
+                        end_time=start_time, beamline_id=beamline_id, scan_id=scan_id,
+                        custom=custom, status=status).save(wtimeout=100,write_concern={'w': 1})
+    except:
+        metadataLogger.logger.warning('Header cannot be created')
+        raise
     return header
 
 
 def list_headers():
+    """
+    Return all Header instances created
+    """
     try:
         headers = Header.objects.all()
     except:
@@ -40,6 +59,12 @@ def list_headers():
 
 
 def get_header_object(id):
+    """
+    Return the Header object with given id
+    
+    :param id: Header _id
+    :type id: pymongo.ObjectId instance
+    """
     try:
         header_object = Header.objects(_id=id)
     except:
@@ -47,37 +72,84 @@ def get_header_object(id):
     return header_object
 
 
-def insert_event_descriptor(event_descriptor_id, header_id, event_type_id, event_type_name=None, type_descriptor=dict(),
+def get_header_id(scan_id):
+    """
+    Retrieve Header _id given scan_id
+
+    :param scan_id: scan_id for a run header
+    :type scan_id: int
+
+    """
+    collection = Header._get_collection()
+    try:
+        hdr_crsr = collection.find({'scan_id': scan_id}).limit(1)
+    except:
+        raise
+    if hdr_crsr.count() == 0:
+        raise Exception('header_id cannot be retrieved. Please validate scan_id')
+    result = hdr_crsr[0]['_id']
+    hdr_crsr.close()
+    return result
+
+
+def insert_event_descriptor(scan_id, event_type_id, event_type_name=None, type_descriptor=dict(),
                             tag=None):
     """
+    Create event_descriptor entries that serve as descriptors for given events that are part of a run header
+    
+    :param scan_id: Consumer specified id for a specific scan
+    :type scan_id: int
+    :param event_type_id: Simple identifier for a given event type. Refer to api documentation for specific event types
+    :type event_type_id: int
+    :param event_type_name: Name information for a given event
+    :type event_type_name: str
+    :param type_descriptor: Additional user/data collection define attribute-value pairs
+    :type type_descriptor: dict
+    :param tag: Provides information regarding nature of event
+    :type tag: str
+
     >>> insert_event_descriptor(event_descriptor_id=134, header_id=1345, event_type_id=0, event_type_name='scan')
     >>> insert_event_descriptor(event_descriptor_id=134, header_id=1345, event_type_id=0, event_type_name='scan',
     ... type_descriptor={'custom_field': 'value', 'custom_field2': 'value2'})
     >>> insert_event_descriptor(event_descriptor_id=134, header_id=1345, event_type_id=0, event_type_name='scan',
     ... type_descriptor={'custom_field': 'value', 'custom_field2': 'value2'}, tag='analysis')
     """
-    if get_event_descriptor_object(event_descriptor_id):
-        raise ValueError('EventDescriptor with given event_descriptor_id exists')
-    else:
-        try:
-            event_descriptor = EventDescriptor(_id=event_descriptor_id, header_id=header_id, event_type_id=event_type_id,
-                                               event_type_name=event_type_name, type_descriptor=type_descriptor,
-                                               tag=tag).save(wtimeout=100, write_concern={'w': 1})
-        except:
-            metadataLogger.logger.warning('EventDescriptor cannot be created')
-            raise
+    #if get_event_descriptor_object(event_descriptor_id):
+    #    raise ValueError('EventDescriptor with given event_descriptor_id exists')
+    #else:
+    header_id = get_header_id(scan_id)
+    try:
+        event_descriptor = EventDescriptor(header_id=header_id, event_type_id=event_type_id,
+                                           event_type_name=event_type_name, type_descriptor=type_descriptor,
+                                           tag=tag).save(wtimeout=100, write_concern={'w': 1})
+    except:
+        metadataLogger.logger.warning('EventDescriptor cannot be created')
+        raise
     return event_descriptor
 
 
-def get_event_descriptor_object(id):
+def get_event_descriptor_hid_edid(name, s_id):
+    """
+    Returns EventDescriptor instance given _id
+
+    :param id: Unique identifier for EventDescriptor instance (refers to _id in mongodb schema)
+    :type id: int
+
+    """
+    header_id = get_header_id(s_id)
     try:
-        event_descriptor_object = EventDescriptor.objects(_id=id)
+        event_descriptor_coll = EventDescriptor._get_collection()
+        result = event_descriptor_coll.find({'event_type_name': name, 'header_id': header_id}).limit(1)
     except:
         raise
-    return event_descriptor_object
+    res = result[0]
+    return res['header_id'], res['_id']
 
 
 def list_event_descriptors():
+    """
+    Returns all EventDescriptor instances saved into the database
+    """
     try:
         event_descriptors = EventDescriptor.objects.all()
     except:
@@ -86,19 +158,31 @@ def list_event_descriptors():
     return event_descriptors
 
 
-def insert_event(event_id, header_id, descriptor_id, description=None, owner=getpass.getuser(), seq_no=None,
+def insert_event(scan_id, descriptor_name, description=None, owner=getpass.getuser(), seq_no=None,
                  data=dict()):
-    descriptor_list = get_event_descriptor_object(descriptor_id)
-    if not descriptor_list:
-        metadataLogger.logger.warning('EventDescriptor cannot be located. Check descriptor_id')
-        raise ValueError('EventDescriptor cannot be located. Check descriptor_id')
-    else:
-        try:
-            event = Event(_id=event_id, header_id=header_id, event_descriptor_id=descriptor_id, description=description,
-                          owner=owner, seq_no=seq_no, data=data).save(wtimeout=100, write_concern={'w': 1})
-        except:
-            metadataLogger.logger.warning('Event cannot be recorded')
-            raise
+    """
+    Create event entries that record experimental data 
+    
+    :param descriptor_name: EventDescriptor name that contains info regarding set of events to be saved
+    :type descriptor_name: str
+    :param description: User generated optional string to be used as descriptors
+    :type description: str
+    :param owner: Event owner (default: debian session owner)
+    :type owner: str
+    :param seq_no: specifies the event's place in data sequence within a the event descriptor set
+    :type seq_no: int
+    :param data: Data Collection routine defined name-value 
+    :type data: dict
+    :Raises: TypeError, OperationError, ConnectionFailure
+    :returns: Event object
+    """
+    header_id, descriptor_id = get_event_descriptor_hid_edid(descriptor_name, scan_id)
+    try:
+        event = Event(event_descriptor_id=descriptor_id, header_id=header_id, description=description,
+                      owner=owner, seq_no=seq_no, data=data).save(wtimeout=100, write_concern={'w': 1})
+    except:
+        metadataLogger.logger.warning('Event cannot be recorded')
+        raise
     return event
 
 
@@ -158,7 +242,7 @@ def update_header_status(header_id, status):
         raise
 
 
-def find(header_id=None, scan_id=None, owner=None, start_time=None, beamline_id=None, end_time=None, contents=False,
+def find(header_id=None, scan_id=None, owner=None, start_time=None, beamline_id=None, end_time=None, data=False,
          **kwargs):
     """
     Find by event_id, beamline_config_id, header_id. As of MongoEngine 0.8 the querysets utilise a local cache.
@@ -173,39 +257,45 @@ def find(header_id=None, scan_id=None, owner=None, start_time=None, beamline_id=
      >>> find(header_id=[130,123,145,247,...])
      >>> find(header_id={'start': 129, 'end': 141})
      >>> find(start_time=date.datetime(2014, 6, 13, 17, 51, 21, 987000)))
-      >>> find(start_time=date.datetime(2014, 6, 13, 17, 51, 21, 987000)))
-      >>> find(start_time={'start': datetime.datetime(2014, 6, 13, 17, 51, 21, 987000),
-                            'end': datetime.datetime(2014, 6, 13, 17, 51, 21, 987000)})
+     >>> find(start_time=date.datetime(2014, 6, 13, 17, 51, 21, 987000)))
+     >>> find(start_time={'start': datetime.datetime(2014, 6, 13, 17, 51, 21, 987000),
+                      ... 'end': datetime.datetime(2014, 6, 13, 17, 51, 21, 987000)})
       >>> find(event_time=datetime.datetime(2014, 6, 13, 17, 51, 21, 987000)
       >>> find(event_time={'start': datetime.datetime(2014, 6, 13, 17, 51, 21, 987000})
     """
     supported_wildcard = ['*', '.', '?', '/', '^']
     query_dict = dict()
-    header = None
-    events = None
     try:
         coll = Header._get_collection()
     except:
         metadataLogger.logger.warning('Collection Header cannot be accessed')
         raise
-    if header_id is 'current':
-        header_cursor = coll.find().sort([('end_time', -1)]).limit(5)
+    if scan_id is 'current':
+        header_cursor = coll.find().sort([('end_time', -1)]).limit(1)
         header = header_cursor[0]
-        if contents is True:
-            event_desc = find_event_descriptor(header['_id'])
-            for e_d in event_desc:
-                header['event_descriptor_' + str(e_d['_id'])] = e_d
-                events = find_event(event_descriptor_id=e_d['_id'])
-                header['event_descriptor_' + str(e_d['_id'])]['events'] = __decode_cursor(events)
-    elif header_id is 'last':
+        event_desc = find_event_descriptor(header['_id'])
+        i = 0
+        for e_d in event_desc:
+            header['event_descriptor_' + str(i)] = e_d
+            events = find_event(event_descriptor_id=e_d['_id'])
+            if data is True:
+                header['event_descriptor_' + str(i)]['events'] = __decode_cursor(events)
+                i += 1
+            else:
+                i += 1
+    elif scan_id is 'last':
         header_cursor = coll.find().sort([('end_time', -1)]).limit(5)
         header = header_cursor[1]
-        if contents is True:
-            event_desc = find_event_descriptor(header['_id'])
-            for e_d in event_desc:
-                header['event_descriptor_' + str(e_d['_id'])] = e_d
-                events = find_event(event_descriptor_id=e_d['_id'])
-                header['event_descriptor_' + str(e_d['_id'])]['events'] = __decode_cursor(events)
+        event_desc = find_event_descriptor(header['_id'])
+        i = 0
+        for e_d in event_desc:
+            header['event_descriptor_' + str(i)] = e_d
+            events = find_event(event_descriptor_id=e_d['_id'])
+            if data is True:
+                header['event_descriptor_' + str(i)]['events'] = __decode_cursor(events)
+                i += 1
+            else:
+                i += 1
     else:
         if header_id is not None:
             query_dict['_id'] = header_id
@@ -243,6 +333,19 @@ def find(header_id=None, scan_id=None, owner=None, start_time=None, beamline_id=
                     query_dict['end_time'] = {'$gte': end_time,
                                               '$lt': datetime.datetime.utcnow()}
         header = __decode_hdr_cursor(find_header(query_dict))
+        #TODO: For each header within the returned results, add event_desc field
+        hdr_keys = header.keys()
+        for key in hdr_keys:
+            event_desc = find_event_descriptor(header[key]['_id'])
+            i = 0
+            for e_d in event_desc:
+                header[key]['event_descriptor_' + str(i)] = e_d
+                if data is True:
+                    events = find_event(event_descriptor_id=e_d['_id'])
+                    header[key]['event_descriptor_' + str(i)]['events'] = __decode_cursor(events)
+                    i += 1
+                else:
+                    i += 1
     return header
 
 
@@ -262,10 +365,19 @@ def __decode_hdr_cursor(cursor_object):
     return headers
 
 
+def __decode_e_d_cursor(cursor_object):
+    event_descriptors = dict()
+    for temp_dict in cursor_object:
+        event_descriptors['event_descriptor_' + str(temp_dict['_id'])] = temp_dict
+    return event_descriptors
+
+
 def __decode_cursor(cursor_object):
     events = dict()
+    i = 0
     for temp_dict in cursor_object:
-        events[temp_dict['_id']] = temp_dict
+        events['event_' + str(i)] = temp_dict
+        i += 1
     return events
 
 
