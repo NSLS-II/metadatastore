343b6aeb2c376d4198c568401d9286183cd23672
arkilic <arkilic@bnl.gov>
2014-08-06 11:10:42 -0400

    userapi.commands updated for the new schema. Sample code added for all apis

--- metadataStore/userapi/commands.py
+++ metadataStore/userapi/commands.py
@@ -5,135 +5,147 @@ from metadataStore.dataapi.raw_commands import *
 #TODO: Use whoosh to add "did you mean ....?" for misspells
 #TODO: Make datetime user friendly
 
-def create(entry_type, **kwargs):
+
+def create(header=None, beamline_config=None, event_descriptor=None):
     """
-    Create allows creation of a run_header and beamline_config. Routine must be provided with dictionary of dictionaries
-    with keys header and beamline_config as shown below:
-    >>> create(entry_type='header', header_id=1345, scan_id=14, custom={'field1': 'value1', 'field2': 'value2'})
-    >>> create(entry_type='header', header_id=1345, owner='arkilic', scan_id=14,
-            ...custom={'field1': 'value1', 'field2': 'value2'})
-    >>> create(entry_type='header', header_id=1345, owner='arkilic', scan_id=14, start_time=datetime.datetime.utcnow(),
-            ...custom={'field1': 'value1', 'field2': 'value2'})
-    EventDescriptor Creation
-    >>> create(entry_type='event_descriptor', header_id=1345, event_type_id=1, event_type_name='scan')
-    >>> create(entry_type='event_descriptor', header_id=1345, event_type_id=1, event_type_name='scan',
-           ... type_descriptor={'attribute': 'value'})
-    BeamlineConfig Creation
-    >>> create(entry_type='beamline_config', header_id=1345)
-    >>> create(entry_type='beamline_config', header_id=1345, config_params={'attribute': 'value', 'attributex': 'valuex'})
+    Create header, beamline_config, and event_descriptor
+
+    :param header: Header attribute-value pairs
+    :type header: dict
+    :param beamline_config: BeamlineConfig attribute-value pairs
+    :type beamline_config: dict
+    :param event_descriptor: EventDescriptor attribute-value pairs
+    :type event_descriptor: dict
+
+    :Raises: TypeError, ValueError, ConnectionFailure, NotUniqueError
+
+     :returns: None
+
     """
-    if entry_type is 'header':
-        if kwargs.has_key('header_id'):
-            _id = kwargs['header_id']
+    if header is not None:
+        if isinstance(header, dict):
+            if 'scan_id' in header:
+                scan_id = header['scan_id']
+            else:
+                raise ValueError('scan_id is a required field')
+            if 'start_time' in header:
+                start_time = header['start_time']
+            else:
+                start_time = datetime.datetime.utcnow()
+            if 'owner' in header:
+                owner = header['owner']
+            else:
+                owner = getpass.getuser()
+            if 'beamline_id' in header:
+                beamline_id = header['beamline_id']
+            else:
+                beamline_id = None
+            if 'custom' in header:
+                custom = header['custom']
+            else:
+                custom = dict()
+            if 'status' in header:
+                status = header['status']
+            else:
+                status = 'In Progress'
+            try:
+                save_header(scan_id=scan_id, header_owner=owner, start_time=start_time, beamline_id=beamline_id,
+                            status=status, custom=custom)
+            except:
+                raise
         else:
-            raise ValueError('header_id is required for run header creation')
-        if kwargs.has_key('scan_id'):
-            scan_id = kwargs['scan_id']
+            raise TypeError('Header must be a Python dictionary ')
+
+    if beamline_config is not None:
+        if isinstance(beamline_config, dict):
+            if 'scan_id' in beamline_config:
+                scan_id = beamline_config['scan_id']
+            else:
+                raise ValueError('scan_id is a required field')
+            if 'config_params' in beamline_config:
+                config_params = beamline_config['config_params']
+            else:
+                config_params = dict()
+            try:
+                save_beamline_config(scan_id=scan_id, config_params=config_params)
+            except:
+                raise
         else:
-            raise ValueError('scan_id is required for run header creation')
-        if kwargs.has_key('owner'):
-            header_owner = kwargs['owner']
+            raise TypeError('BeamlineConfig must be a Python dictionary')
+
+    if event_descriptor is not None:
+        if isinstance(event_descriptor, dict):
+            if 'scan_id' in event_descriptor:
+                scan_id = event_descriptor['scan_id']
+            else:
+                raise ValueError('scan_id is required for EventDescriptor entries')
+            if 'event_type_id' in event_descriptor:
+                event_type_id = event_descriptor['event_type_id']
+            else:
+                event_type_id = None
+            if 'event_type_name' in event_descriptor:
+                event_type_name = event_descriptor['event_type_name']
+            else:
+                raise ValueError('event_type_name is required for EventDescriptor')
+            if 'type_descriptor' in event_descriptor:
+                type_descriptor = event_descriptor['type_descriptor']
+            else:
+                type_descriptor = dict()
+            if 'tag' in event_descriptor:
+                tag = event_descriptor['tag']
+            else:
+                tag = None
+            try:
+                insert_event_descriptor(scan_id=scan_id, event_type_id=event_type_id, event_type_name=event_type_name,
+                                        type_descriptor=type_descriptor, tag=tag)
+            except:
+                raise
         else:
-            header_owner = getpass.getuser()
-        if kwargs.has_key('start_time'):
-            start_time = kwargs['start_time']
-        else:
-            start_time = datetime.datetime.utcnow()
-        if kwargs.has_key('beamline_id'):
-            beamline_id = kwargs['beamline_id']
-        else:
-            beamline_id = None
-        if kwargs.has_key('custom'):
-            custom = kwargs['custom']
-        else:
-            custom = dict()
-        status = 'In Progress'
-        save_header(header_id=_id, scan_id=scan_id, header_owner=header_owner, start_time=start_time,
-                    beamline_id=beamline_id, status=status, custom=custom)
-    elif entry_type is 'event_descriptor':
-        if kwargs.has_key('event_descriptor_id'):
-            _id = kwargs['event_descriptor_id']
-        else:
-            raise ValueError('event_descriptor_id is a required field for event_descriptor')
-        if kwargs.has_key('header_id'):
-            h_id = kwargs['header_id']
-        else:
-            raise ValueError('header_id is a required field for event_descriptor')
-        if kwargs.has_key('event_type_id'):
-            ev_type_id = kwargs['event_type_id']
-        else:
-            raise ValueError('event_type_id is a required field for event_descriptor')
-        if kwargs.has_key('event_type_name'):
-            ev_type_name = kwargs['event_type_name']
-        else:
-            ev_type_name = None
-        if kwargs.has_key('type_descriptor'):
-            type_descriptor = kwargs['type_descriptor']
-        else:
-            type_descriptor = dict()
-        if kwargs.has_key('tag'):
-            tag = kwargs['tag']
-        else:
-            tag = None
-        insert_event_descriptor(event_descriptor_id=_id, header_id=h_id, event_type_id=ev_type_id,
-                                event_type_name=ev_type_name, type_descriptor=type_descriptor, tag=tag)
-    elif entry_type is 'beamline_config':
-        if kwargs.has_key('beamline_cfg_id'):
-            b_c_id = kwargs['beamline_cfg_id']
-        else:
-            raise ValueError('beamline_cfg_id is required to create beamline_config entry')
-        if kwargs.has_key('header_id'):
-            h_id = kwargs['header_id']
-        else:
-            raise ValueError('header_id is required to create beamline_config entry')
-        if kwargs.has_key('config_params'):
-            config_params = kwargs['config_params']
-        else:
-            config_params = dict()
-        save_beamline_config(beamline_cfg_id=b_c_id, header_id=h_id, config_params=config_params)
-    else:
-        raise ValueError(str(entry_type) + ' is not a valued entry type')
+            raise TypeError('EventDescriptor must be a Python dictionary')
 
 
-def record( event_id, descriptor_id, header_id='current', owner=getpass.getuser(), seq_no=None, data={}):
+def record(scan_id, descriptor_name, seq_no, owner=getpass.getuser(), data=dict(), description=None):
     """
-    Creates an event entry to save experimental/data analysis progress. metadataStore can be used standalone without/
-    dataBroker.
-    :param event_id: Unique mongodb _id field assigned to a given event
-    :type event_id: int
-    :param header_id: Run_header _id that event belongs to
-    :type header_id: int
-    :param event_descriptor_id: int
-    :type event_descriptor_id
-    :param seq_no: int
-    :type seq_no:
-    :param start_time: formatted datetime of event
-    :type start_time: datetime
-    :param end_time: formatted datetime
-    :type end_time: datetime
+    Events are saved given scan_id and descriptor name and additional optional parameters
 
-    *Usage:*
-    >>> record(event_id=134, event_type_id=0, )
+    :param scan_id: Unique run identifier
+    :type scan_id: int
+    :param descriptor_name: EventDescriptor that serves as an Event header
+    :type descriptor_name: str
+    :param seq_no: Data point sequence number
+    :type seq_no: int
+    :param owner: Run owner(default: unix session owner)
+    :type owner: str
+    :param data: Serves as an experimental data storage structure
+    :type data: dict
 
-    :returns: None
+
+    :Raises: ConnectionFailure, NotUniqueError, ValueError
+
+    Required fields: scan_id, descriptor_name, seq_no
+    Optional fields: owner, data, description
+
+    Usage:
+
+    >>> record(scan_id=135, descriptor_name='some_scan', seq_no=0)
+    >>> record(scan_id=135, descriptor_name='some_scan', seq_no=1, owner='arkilic')
+    >>> record(scan_id=135, descriptor_name='some_scan', seq_no=2, data={'name': 'value'})
+    >>> record(scan_id=135, descriptor_name='some_scan', seq_no=2, data={'name': 'value'},
+           ... description='some entry')
     """
-    #TODO: Make time range search user friendly: replace datetime with string time formatting
 
     try:
-        insert_event(event_id=event_id, header_id=header_id, descriptor_id=descriptor_id,owner=owner,
-                     seq_no=seq_no, data=data)
-    except OperationError:
+        insert_event(scan_id=scan_id, descriptor_name=descriptor_name, owner=owner, seq_no=seq_no, data=data,
+                     description=description)
+    except:
         raise
 
 
-def search(header_id=None, owner=None, start_time=None, end_time=None, scan_id=None,
-           contents=False, **kwargs):
+def search(owner=None, start_time=None, end_time=None, scan_id=None, data=False):
     #TODO: Modify according to changes in log() and raw_commands
     #TODO: Make time range search user friendly: replace datetime with string time formatting
-    #TODO: Add tag search
     try:
-        result = find(header_id=header_id, owner=owner, start_time=start_time, end_time=end_time,
-                      scan_id=scan_id, contents=contents, **kwargs)
+        result = find(owner=owner, start_time=start_time, end_time=end_time, scan_id=scan_id, data=data)
     except OperationError:
         raise
     return result
