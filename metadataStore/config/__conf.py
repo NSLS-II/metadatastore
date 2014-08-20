cd3dda51bf9e8edc8afded3ccdc2665026f9f60c
arkilic <arkilic@bnl.gov>
2014-08-20 16:48:21 -0400

    Fixed

--- metadataStore/config/__conf.py
+++ metadataStore/config/__conf.py
@@ -2,15 +2,29 @@ __author__ = 'arkilic'
 
 import os.path
 import ConfigParser
+from os import path
 
+
+def check_config_file():
+    result = False
+    if path.isfile('/etc/dataBroker.conf'):
+        result = True
+    if path.isfile(os.path.expanduser('~/dataBroker.conf')):
+        result = True
+    if path.isfile('dataBroker.conf'):
+        result = True
+    return result
 
 def __loadConfig():
     cf=ConfigParser.SafeConfigParser()
-    cf.read([
-        '/etc/dataBroker.conf',
-        os.path.expanduser('~/dataBroker.conf'),
-        'dataBroker.conf'
-    ])
+    if check_config_file():
+        cf.read([
+            '/etc/dataBroker.conf',
+            os.path.expanduser('~/dataBroker.conf'),
+            'dataBroker.conf'
+        ])
+    else:
+        raise IOError('Configuration file does not exist')
     return cf
 
 
