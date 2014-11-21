fa55e65dbde71d6fe73ab922746bb225506d62af
Eric Dill <edill@bnl.gov>
2014-11-21 16:12:10 -0500

    MNT: Implemented record with recursion

--- metadataStore/collectionapi/commands.py
+++ metadataStore/collectionapi/commands.py
@@ -199,7 +199,7 @@ def create(header=None, beamline_config=None, event_descriptor=None):
             raise TypeError('EventDescriptor must be a Python dictionary')
 
 
-def record(event=dict()):
+def record(event):
     """
 
     Events are saved given scan_id and descriptor name and additional optional parameters.
@@ -233,7 +233,7 @@ def record(event=dict()):
         if 'descriptor_name' in event:
             descriptor_name = event['descriptor_name']
         else:
-            raise ValueError('Descriptor is required in order to record an event')
+            raise ValueError('descriptor_name is required in order to record an event')
         if 'description' in event:
             description = event['description']
         else:
@@ -256,39 +256,51 @@ def record(event=dict()):
         except:
             raise
     elif isinstance(event, list):
-            bulk = db['event'].initialize_ordered_bulk_op()
-            for single_event in event:
-                composed_dict = dict()
-                if 'scan_id' in single_event:
-                    composed_dict['scan_id'] = single_event['scan_id']
-                else:
-                    raise ValueError('scan_id is required in order to record an single_event')
-                if 'descriptor_name' in single_event:
-                    composed_dict['descriptor_name'] = single_event['descriptor_name']
-                else:
-                    raise ValueError('Descriptor is required in order to record an single_event')
-                if 'description' in single_event:
-                    composed_dict['description'] = single_event['description']
-                else:
-                    composed_dict['description'] = None
-                if 'owner' in single_event:
-                    composed_dict['owner'] = single_event['owner']
-                else:
-                    composed_dict['owner'] = getpass.getuser()
-                if 'seq_no' in single_event:
-                    composed_dict['seq_no'] = single_event['seq_no']
-                else:
-                    raise ValueError('seq_no is required field')
-                if 'data' in single_event:
-                    composed_dict['data'] = single_event['data']
-                else:
-                    composed_dict['data'] = dict()
-                header_id, descriptor_id = get_event_descriptor_hid_edid(single_event['descriptor_name'],
-                                                                         single_event['scan_id'])
-                composed_dict['header_id'] = header_id
-                composed_dict['descriptor_id'] = descriptor_id
-                bulk.insert(composed_dict)
-            bulk.execute()
+        # bulk = db['event'].initialize_ordered_bulk_op()
+        errors = []
+        for idx, single_event in enumerate(event):
+            try:
+                record(single_event)
+            except ValueError as ve:
+                errors.append('Event {} of {} raised error: {}. \nEvent: {}'
+                              ''.format(idx, len(event)-1), ve, single_event)
+        if errors:
+            raise ValueError(errors)
+        #     composed_dict = dict()
+        #     if 'scan_id' in single_event:
+        #         composed_dict['scan_id'] = single_event['scan_id']
+        #     else:
+        #         raise ValueError('scan_id is required in order to record an single_event')
+        #     if 'descriptor_name' in single_event:
+        #         composed_dict['descriptor_name'] = single_event['descriptor_name']
+        #     else:
+        #         raise ValueError('Descriptor is required in order to record an single_event')
+        #     if 'description' in single_event:
+        #         composed_dict['description'] = single_event['description']
+        #     else:
+        #         composed_dict['description'] = None
+        #     if 'owner' in single_event:
+        #         composed_dict['owner'] = single_event['owner']
+        #     else:
+        #         composed_dict['owner'] = getpass.getuser()
+        #     if 'seq_no' in single_event:
+        #         composed_dict['seq_no'] = single_event['seq_no']
+        #     else:
+        #         raise ValueError('seq_no is required field')
+        #     if 'data' in single_event:
+        #         composed_dict['data'] = single_event['data']
+        #     else:
+        #         composed_dict['data'] = dict()
+        #     header_id, descriptor_id = get_event_descriptor_hid_edid(single_event['descriptor_name'],
+        #                                                              single_event['scan_id'])
+        #     composed_dict['header_id'] = header_id
+        #     composed_dict['descriptor_id'] = descriptor_id
+        #     bulk.insert(composed_dict)
+        # bulk.execute()
+    else:
+        raise ValueError("Event must be a dict or a list. You provided a {}: "
+                         "{}".format(type(event), event))
+
 
 
 def search(scan_id=None, owner=None, start_time=None, beamline_id=None, end_time=None, data=False,
